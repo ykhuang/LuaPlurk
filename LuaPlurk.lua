dc98dbfb072aa1aa4f440d0206b52cf043015469
YK Huang <ykhuang@gmail.com>
2011-09-08 14:16:30 +0800

    Update README.md and LuaPlurk as v0.1

--- LuaPlurk.lua
+++ LuaPlurk.lua
@@ -1,10 +1,9 @@
----
--- Created by IntelliJ IDEA.
--- User: ying-kai.huang
--- Date: 2011/9/7
--- Time: 上午 11:59
--- To change this template use File | Settings | File Templates.
---
+--[[
+	LuaPlurk:  A Lua implementation of Plurk OAuth
+	Auther: ykhuang@gmail.com
+	Version: 0.1
+]]--
+
 local modname = 'LuaPlurk'
 local LuaPlurk = {}
 _G[modname] = LuaPlurk
@@ -12,13 +11,8 @@ package.loaded[modname] = LuaPlurk
 local _G = _G
 setfenv(1,LuaPlurk)
 
-LuaPlurk = { app_key = nil, app_secret = nil, oauth_token = nil, oauth_token_secret =nil, auth_url = nil, client = nil}
+LuaPlurk = { app_key = nil, app_secret = nil, oauth_token = nil, oauth_token_secret =nil, client = nil}
 local oauth = _G.require "OAuth"
-
-
-function check()
-  _G.print("Check app_key:"..app_key)
-end
 
 function init(key, secret)
   app_key = key
@@ -28,10 +22,10 @@ function init(key, secret)
       AuthorizeUser = {"http://www.plurk.com/OAuth/authorize", method = "GET"},
       AccessToken='http://www.plurk.com/OAuth/access_token'} )
   local values = client:RequestToken()
-  --_G.print(values.oauth_callback_confirmed)
   return values.oauth_callback_confirmed, values.oauth_token, values.oauth_token_secret
 end
 
+-- todo: check if the access token has expired and return status
 function init_client(key, secret, token_key, token_secret)
   app_key = key
   app_secret = secret
@@ -46,7 +40,6 @@ function init_client(key, secret, token_key, token_secret)
 end
 
 function getAuthorizedUrl(token)
-  -- _G.print("Build URL from token: "..token_key.." with client: "..app_key)
   return client:BuildAuthorizationUrl({ oauth_token = token })
 end
 
@@ -66,22 +59,11 @@ function getAccessToken(token, secret, verifier)
   return oauth_token, oauth_token_secret
 end
 
-function plurkRequest(api, token)
---[[
-  client = oauth.new(app_key, app_secret,
-    { RequestToken='http://www.plurk.com/OAuth/request_token',
-      AuthorizeUser = {"http://www.plurk.com/OAuth/authorize", method = "GET"},
-      AccessToken='http://www.plurk.com/OAuth/access_token'
-    })
-	]]--
+function plurkRequest(token, api, args)
 	local api_url = 'http://www.plurk.com'..api
-	_G.print("Get "..api_url.." with client "..app_key)
-
-	local response_code, response_headers, response_status_line, response_body =
-		client:PerformRequest("POST", api_url, {oauth_token = token})
-	_G.print("response_code", response_code)
-	_G.print("response_status_line", response_status_line)
-	for k,v in _G.pairs(response_headers) do _G.print(k,v) end
-	_G.print("response_body", response_body)
-	
+	if args == nil then
+		args = {}
+	end
+	args['oauth_token'] = token
+	return client:PerformRequest("POST", api_url, args)
 end
--- README.md
+++ README.md
@@ -2,16 +2,47 @@
 This is a Plurk API 2.0 implementation based on Lua. Ignacia's LuaOAuth is used to do the OAuth1.0 part.
 
 ## Lua packages dependencies #
-Lua 5.1.4
 
+Lua 5.1.4
 LuaOAuth by Ignacio: https://github.com/ignacio/LuaOAuth
 
-luacrypto
+LuaOAuth support async and sync mode. Only synchrous mode is support so far.
 
+LuaOAuth depends on several packages:
+luacrypto
 LuaSocket
+LuaNode by Ignacio (for async mode)
 
 ## Usage and examples #
 
+To request token:
+``` lua
+-- get request token
+local ret, rtoken, rtoken_secret = papi.init(app_key, app_secret)
+local aurl = papi.getAuthorizedUrl(rtoken)
+-- Prompt user to authorize
+print("Enter PIN verifier:")
+local verifier = assert(io.read("*n"))
+verifier = tostring(verifier)
+local access_token, access_token_secret = papi.getAccessToken(rtoken, rtoken_secret, verifier)
+```
+
+To access Plurk API
+``` lua
+-- use init_client if you already has access token
+papi.init_client(app_key, app_secret, token, token_secret)
+local api_url = '/APP/Profile/getPublicProfile'
+local api_args = {user_id='whoever'}
+local response_code, response_headers, response_status_line, response_body =
+	papi.plurkRequest(token, api_url, api_args)
+```
+
 ## Reference #
 
+
 ## Changelog #
+2011, Sep. 8: Upload first LuaPlurk and goes v0.1.
+
+
+## Todo #
+* Add token expiration check.
